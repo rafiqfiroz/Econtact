5f74575fd5698e90f3a6d551cce837cd133ce395
rafiqfiroz <74096632+rafiqfiroz@users.noreply.github.com>
2023-05-11 13:04:22 +0600

    Add project files.

---
+++ Econtact/Data/ContactClass.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Econtact.Data
+{
+    public class ContactClass
+    {
+        public int ContactId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string ContactNo { get; set; }
+        public string Address { get; set; }
+        public string Gender { get; set; }
+        static string myconnstring = ConfigurationManager.ConnectionStrings["connstring"].ConnectionString; 
+        //Selecting Data from Database
+        public DataTable Select()
+        {
+            //Make Connectionstring
+            SqlConnection conn = new SqlConnection(myconnstring);
+            DataTable dt = new DataTable();
+            try
+            {
+                //wrinting sql query
+                string sql = "Select * From tbl_Contact";
+                SqlCommand cmd= new SqlCommand(sql,conn);
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                conn.Open();
+                adapter.Fill(dt);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                conn.Close(); 
+            }
+            return dt;
+        }
+        //Inserting Data into Database
+        public bool Insert(ContactClass contact)
+        {
+            //creating a default return type and settings its value to false
+
+            bool isSuccess=false;
+            SqlConnection conn = new SqlConnection(myconnstring);
+            try
+            {
+                string sql = "Insert into tbl_Contact(FirstName,LastName,ContactNo,Address,Gender)Values(@FirstName,@LastName,@ContactNo,@Address,@Gender)";
+                SqlCommand cmd=new SqlCommand(sql,conn);
+                cmd.Parameters.AddWithValue("@FirstName",contact.FirstName);
+                cmd.Parameters.AddWithValue("@LastName", contact.LastName);
+                cmd.Parameters.AddWithValue("@ContactNo", contact.ContactNo);
+                cmd.Parameters.AddWithValue("@Address", contact.Address);
+                cmd.Parameters.AddWithValue("@Gender", contact.Gender);
+                //connection open
+                conn.Open();
+                int rows=cmd.ExecuteNonQuery();
+                if (rows>0)
+                {
+                    isSuccess = true;
+                }
+                else
+                {
+                    isSuccess= false;
+                }
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }   
+            return isSuccess;
+        }
+        public bool Update(ContactClass contact)
+        {
+            bool isSuccess=false;
+            SqlConnection conn = new SqlConnection(myconnstring);
+            try
+            {
+                string sql = "Update tbl_Contact Set FirstName=@FirstName,LastName=@LastName,ContactNo=@ContactNo,Address=@Address,Gender=@Gender";
+                SqlCommand cmd=new SqlCommand(sql,conn);
+                cmd.Parameters.AddWithValue("@FirstName", contact.FirstName);
+                cmd.Parameters.AddWithValue("@LastName", contact.LastName);
+                cmd.Parameters.AddWithValue("@ContactNo", contact.ContactNo);
+                cmd.Parameters.AddWithValue("@Address", contact.Address);
+                cmd.Parameters.AddWithValue("@Gender", contact.Gender);
+                conn.Open();
+                int rows=cmd.ExecuteNonQuery();
+                if (rows>0)
+                {
+                    isSuccess = true;
+                }
+                else
+                {
+                    isSuccess=false;
+                }
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally 
+            { 
+                conn.Close(); 
+            }
+            return isSuccess;
+        }
+        public bool Delete(ContactClass contact) 
+        {
+            bool isSuccess=false;
+            SqlConnection conn = new SqlConnection(myconnstring);
+            try
+            {
+                string sql = "Delete from tbl_Contact where ContactId=@ContactId";
+                SqlCommand cmd= new SqlCommand(sql,conn);
+                cmd.Parameters.AddWithValue("ContactId",contact.ContactId);
+                conn.Open();
+                int rows=cmd.ExecuteNonQuery();
+                if (rows>0) 
+                {
+                    isSuccess = true;
+                }
+                else
+                {
+                    isSuccess=false;
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally 
+            { 
+                conn.Close();
+            }
+            return isSuccess;
+        }
+       
+    }
+}
---
+++ Econtact/Form1.cs
@@ -0,0 +1,108 @@
+﻿using Econtact.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Econtact
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        ContactClass c=new ContactClass();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            c.FirstName=textBox2.Text;
+            c.LastName=textBox3.Text;
+            c.ContactNo=textBox4.Text;
+            c.Address=textBox5.Text;
+            c.Gender = comboBox1.Text;
+            bool success = c.Insert(c);
+            if (success==true)
+            {
+                MessageBox.Show("Data insert successfully");
+                Clear();
+            }
+            else
+            {
+                MessageBox.Show("Please inter valid data");
+            }
+            //Load Data into Grid View
+            DataTable dt= c.Select();
+            dataGridView1.DataSource = dt;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DataTable dt = c.Select();
+            dataGridView1.DataSource = dt;
+        }
+        public void Clear()
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+            
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            c.FirstName = textBox2.Text;
+            c.LastName = textBox3.Text;
+            c.ContactNo = textBox4.Text;
+            c.Address = textBox5.Text;
+            c.Gender = comboBox1.Text;
+            bool success = c.Update(c);
+            if (success == true)
+            {
+                MessageBox.Show("Data Update successfully");
+                Clear();
+            }
+            else
+            {
+                MessageBox.Show("Please inter valid data");
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int rowIndex = e.RowIndex;
+            textBox1.Text = dataGridView1.Rows[rowIndex].Cells[0].Value.ToString();
+            textBox2.Text = dataGridView1.Rows[rowIndex].Cells[1].Value.ToString();
+            textBox3.Text = dataGridView1.Rows[rowIndex].Cells[2].Value.ToString();
+            textBox4.Text = dataGridView1.Rows[rowIndex].Cells[3].Value.ToString();
+            textBox5.Text = dataGridView1.Rows[rowIndex].Cells[4].Value.ToString();
+            comboBox1.Text = dataGridView1.Rows[rowIndex].Cells[4].Value.ToString();
+
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            c.ContactId = Convert.ToInt32(textBox1.Text);
+            bool success = c.Delete(c);
+            if (success==true)
+            {
+                MessageBox.Show("Delete successfully");
+            }
+            else
+            {
+                MessageBox.Show("Failed to delete");
+            }
+        }
+    }
+}
